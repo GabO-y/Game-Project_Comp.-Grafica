d6f18efc2c8e68ef6ea805216a22b3112881d069
O_Gabriel <gabrielosilva1000@gmail.com>
2025-10-28 23:51:32 -0300

    perseguição dos items ao plauer

--- Cenas/Item/item.gd
+++ Cenas/Item/item.gd
@@ -10,6 +10,7 @@ var dist = 1
 var type
 
 var is_to_chase_player = false
+var curve: MyCurve
 
 func _ready() -> void:
 	
@@ -18,27 +19,38 @@ func _ready() -> void:
 	z_index = 100
 	process_mode = Node.PROCESS_MODE_ALWAYS
 	
-
 func _process(delta: float) -> void:
 	if is_to_chase_player:
 		chase_player()
 		
+# Pega um ponto da curva e atualiza para a nova pos do player
 func chase_player():
+	var p = curve.get_point_by_progress()
+	position = p
+	curve.set_p2(Globals.player_pos)
 	
-	var dir = global_position.direction_to(Globals.player_pos).normalized()
-	
-	if global_position.distance_to(Globals.player_pos) < dist:
-		dir = Vector2.ZERO
-			
-	position += dir * 3
-	
+# Gera a curva que o item vai seguir ate o player
 func start_chase_player():
-#   Sopostamente a logica de fazer uma curva antes de ir pro player, dps ir pro player
-
-	is_to_chase_player = true
-
-	print("start_chase_player: Item, precisa terminar")
 	
+	is_to_chase_player = true
+	
+	var p0 = global_position
+	var p1 = p0
+	var p2 = Globals.player_pos
+	
+	var dir: Vector2 = p0.direction_to(p2) * 80
+	
+	p1 -= dir 
+	
+	dir.x = [-1,1].pick_random()
+	dir.y = [-1,1].pick_random()
+	
+	dir *= 80
+		
+	p1 -= dir
+	
+	curve = MyCurve.new(p0, p1, p2)
+
 func _on_player_body_entered(body: Node2D) -> void:
 	var player = body.get_parent() as Player
 	if player == null: return
